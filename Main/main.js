d2050e8ae0d1c8e3a697407b05c21168b9b75dc1
Ekanshvcpkg <cv16main@gmail.com>
2026-09-20 20:06:38 +0530

    PrintFunc done and searchbtn on working

--- Main/main.js
+++ Main/main.js
@@ -1,23 +1,75 @@
+
  console.log("running main.js");
- // 1 - need to get all the var 
+ // ! 1 - need to get all the var 
 const usernameInput = document.getElementById('username');
 const searchButton = document.getElementById('searchBtn');
-const returnValue = document.getElementById('view');
+const sellectionOpt = document.getElementById('view');
+const dataPrintBase = document.getElementById('output');
+
+ // console.log(sellectionOpt[3]); this works
+
+
 
 let userNameInputValue = "";
+let getFetchData = {}; // obj
+
+const para = document.createElement('p');
 
 
 
+
+
+const textNode  = document.createElement('p');
+
+//! 2 - fetch data 
 async function gitfetch(username) {
-
     const result = fetch(`https://api.github.com/users/${username}`);
     // now i will get the unclean obj
     const unCleanObj = await result;
     return unCleanObj.json(); // async will make it a promise box even if we use await
 }
-
-function searchBtn() { 
+//! 3 - SearchBtn
+async function searchBtn() { 
     userNameInputValue = usernameInput.value.trim();
     // need do something so it become an value not a promise  
-    console.log(gitfetch(userNameInputValue));   
+    getFetchData = await gitfetch(userNameInputValue);  
+    console.log(getFetchData);
+    printData();
 }
+
+//! 4 - PrintFunc
+function printData () { 
+    if (sellectionOpt.value ==="followers") { 
+        console.log("followers");
+        dataPrintBase.innerHTML = "";
+        para.textContent = `Followers-${getFetchData.followers}`
+        dataPrintBase.appendChild(para);
+
+    }else if (sellectionOpt.value ==="following" ) { 
+        console.log("following");
+        dataPrintBase.innerHTML = "";
+        para.textContent = `Following-${getFetchData.following}`
+        dataPrintBase.appendChild(para);
+
+    }else if (sellectionOpt.value ==="repos") { 
+        console.log("repos");
+        dataPrintBase.innerHTML = "";
+        para.textContent = `Followers-${getFetchData.repos}`
+        dataPrintBase.appendChild(para);
+
+    }else if (sellectionOpt.value ==="name") { 
+        console.log("name");
+        dataPrintBase.innerHTML = "";
+        para.textContent = `Name-${getFetchData.name}`
+        dataPrintBase.appendChild(para);
+
+    }else { 
+        console.log("all");
+        dataPrintBase.innerHTML = "";
+        para.textContent = `All-${getFetchData}`
+        dataPrintBase.appendChild(para);
+    }
+}
+
+
+
--- demon.js
+++ demon.js
@@ -26,11 +26,26 @@
 
 */
 
-async function demon() {
-    let dex = await 7;
-     return dex;
-}
+// async function demon() {
+//     let dex = await 7;
+//     // console.log(dex); // this will give me the value 7 cause it will run after the it has the value of 7 
+//     return dex; 
+// }
 
-demon();
-console.log("C");
+// demon().then(m =>console.log(m)); // this will give me pending promise ok. 
+// but i neeed the value inside the promice what i think it is doing is it is running the console log outside and thing run is is not there the value 
+// so pending to fix it i can use console.log inside the demon some thing like
 
+
+// but now the problem is that i need 7 and value not the obj ok 
+//demon().then(m =>console.log(m)) using this i am able to get the value but is that the only methold ?? 
+
+
+const user = { followers: 50, key: "surprise!" };
+const key = "followers";
+
+console.log(user.key); // got this 
+console.log(user[key]);// wtf is that bro why it is printing 50 man wtf ? 
+// key = followers so it is a key ok. 
+// user.followers is that igt is doing ? cause in know that if you want to add a key value pare in some obj you do 
+// user[key] = value;
