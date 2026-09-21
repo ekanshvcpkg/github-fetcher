04096d65bcc534d52aaecf2c052dd5504aeb3c93
Ekanshvcpkg <cv16main@gmail.com>
2026-09-21 11:54:57 +0530

    now making git aura battle

--- Main/main.js
+++ Main/main.js
@@ -2,22 +2,22 @@
  console.log("running main.js");
  // ! 1 - need to get all the var 
 const usernameInput = document.getElementById('username');
+const usernameInput2 = document.getElementById('username2');
 const searchButton = document.getElementById('searchBtn');
 const sellectionOpt = document.getElementById('view');
 const dataPrintBase = document.getElementById('output');
 
  // console.log(sellectionOpt[3]); this works
 
+let userNameInputValueUser1 = "";
+let userNameInputValueUser2 = "";
+let getFetchDataForUser1 = {}; // obj
+let getFetchDataForUser2 = {}; // obj
 
-
-let userNameInputValue = "";
-let getFetchData = {}; // obj
 
 const para = document.createElement('p');
 
-
-
-
+const options = ["name" , "followers" , "following" , "location","public_repos","created_at","updated_at"];
 
 const textNode  = document.createElement('p');
 
@@ -30,10 +30,14 @@ async function gitfetch(username) {
 }
 //! 3 - SearchBtn
 async function searchBtn() { 
-    userNameInputValue = usernameInput.value.trim();
+    // ? run only when the user has new name 
+    if (usernameInput) { 
+            userNameInputValue = usernameInput.value.trim();
     // need do something so it become an value not a promise  
     getFetchData = await gitfetch(userNameInputValue);  
     console.log(getFetchData);
+    }
+
     printData();
 }
 
@@ -52,9 +56,9 @@ function printData () {
         dataPrintBase.appendChild(para);
 
     }else if (sellectionOpt.value ==="repos") { 
-        console.log("repos");
+        console.log("location");
         dataPrintBase.innerHTML = "";
-        para.textContent = `Followers-${getFetchData.repos}`
+        para.textContent = `Location-${getFetchData.location}`
         dataPrintBase.appendChild(para);
 
     }else if (sellectionOpt.value ==="name") { 
@@ -63,13 +67,39 @@ function printData () {
         para.textContent = `Name-${getFetchData.name}`
         dataPrintBase.appendChild(para);
 
-    }else { 
-        console.log("all");
+    }else if (sellectionOpt.value ==="public_repos") { 
         dataPrintBase.innerHTML = "";
-        para.textContent = `All-${getFetchData}`
+        para.textContent = `Public_repos-${getFetchData.public_repos}`
+        dataPrintBase.appendChild(para);
+
+    }else if (sellectionOpt.value ==="created_at") { 
+        dataPrintBase.innerHTML = "";
+        para.textContent = `Created_at-${getFetchData.created_at}`
+        dataPrintBase.appendChild(para);
+
+    }else if (sellectionOpt.value ==="updated_at") { 
+        dataPrintBase.innerHTML = "";
+        para.textContent = `Updated_at-${getFetchData.created_at}`
         dataPrintBase.appendChild(para);
     }
+    else { 
+        console.log("all");
+        dataPrintBase.innerHTML = "";
+        for (let i = 0; i<options.length; i++) { 
+            let temppera = document.createElement('p');
+            temppera.textContent = `${fistoneupper(options[i])}-${getFetchData[options[i]]}`
+            dataPrintBase.appendChild(temppera);
+
+        }
+    }
 }
+
+
+//! 4 toupperfist
+function fistoneupper(userGivevalue) { 
+    return userGivevalue[0].toUpperCase() + userGivevalue.slice(1);
+}
+
 
 
 
